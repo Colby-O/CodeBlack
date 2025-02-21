8240287315ea10faebd04bfedbe1e3e20a5ea3fc
smokeythekat <owr5273@gmail.com>
2025-02-21 18:37:47 -0400

    cart fix

--- Assets/Scripts/Runtime/Cart.cs
+++ Assets/Scripts/Runtime/Cart.cs
@@ -7,38 +7,15 @@ namespace CodeBlack
     public class Cart : MonoBehaviour
     {
         [SerializeField] private LayerMask _itemLayer;
-
-        [SerializeField, ReadOnly] private List<Rigidbody> _items;
+        [SerializeField, ReadOnly] private List<Rigidbody> _items = new();
 
         private void OnCollisionEnter(Collision collision)
         {
             if ((_itemLayer.value & (1 << collision.gameObject.layer)) > 0)
             {
+               collision.rigidbody.isKinematic = true;
+               collision.rigidbody.transform.parent = transform;
                 if (!_items.Contains(collision.rigidbody)) _items.Add(collision.rigidbody);
-                else _items.Remove(collision.rigidbody);
-            }
-        }
-
-        private void OnCollisionExit(Collision collision)
-        {
-            if ((_itemLayer.value & (1 << collision.gameObject.layer)) > 0)
-            {
-                if (!_items.Contains(collision.rigidbody)) _items.Add(collision.rigidbody);
-                else _items.Remove(collision.rigidbody);
-            }
-        }
-
-        private void Awake()
-        {
-            _items = new List<Rigidbody> ();
-        }
-
-        private void Update()
-        {
-            foreach (Rigidbody rb in _items)
-            {
-               rb.isKinematic = true;
-               rb.transform.parent = transform;
             }
         }
     }
