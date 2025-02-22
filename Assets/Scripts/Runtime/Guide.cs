a68f8375369d5e833922ce291be45ffd70573904
smokeythekat <owr5273@gmail.com>
2025-02-22 03:31:59 -0400

    typos

--- Assets/Scripts/Runtime/Guide.cs
+++ Assets/Scripts/Runtime/Guide.cs
@@ -44,23 +44,13 @@ namespace CodeBlack
 
         private HeartCondition _last;
 
-        public void StartHeart()
-        {
-            _heart.Revive();
-        }
-
-        public void StopHeart()
-        {
-            _heart.CauseCardiacArrest(true);
-        }
-
         public void Show(HeartCondition condition)
         {
             _heart.SetHeartRate(58f / 60f);
             _heart.SetAtrialFibrillation(false);
             _heart.SetBlock(0);
             _heart.SetJWave(0);
-            _heart.SetVentricularFibrillation(false, false, true);
+            _heart.SetVentricularFibrillation(false, false);
 
             foreach (GameObject obj in _cures.Values)
             {
@@ -83,8 +73,8 @@ namespace CodeBlack
 
                     _bloodSuger.text = $"<{_settings.bloodSugarDangerousHigh}";
 
-                    _descripton.text = $"A elevated heart rate above 190 BPM. The condition can be caused by the following:\n •\tDiabetes, high blood suger levels greater than {_settings.bloodSugarDangerousHigh} mg/dL. \n •\tPre-existing heart ccondition.\n\nBeing Tachycardic for too long will result in Cardiac Arrest or Atrial Fibrillation. See the respective entries for more infomation about treating these conditions.";
-                    _traetment.text = $"\n• Heart rate can be lowered with Beta-Blockers.\n\n• If blood suger is high treat with insulin.";
+                    _descripton.text = $"A elevated heart rate above 190 BPM. The condition can be caused by the following:\n ï¿½\tDiabetes, high blood sugar levels greater than {_settings.bloodSugarDangerousHigh} mg/dL. \n ï¿½\tPre-existing heart condition.\n\nBeing Tachycardic for too long will result in Cardiac Arrest or Atrial Fibrillation. See the respective entries for more information about treating these conditions.";
+                    _traetment.text = $"\nï¿½ Heart rate can be lowered with Beta-Blockers.\n\nï¿½ If blood sugar is high treat with insulin.";
                     break;
                 case HeartCondition.Bradycardia:
                     _heart.SetHeartRate(58f / 20f);
@@ -92,39 +82,39 @@ namespace CodeBlack
                     _SpO2.text = $">{_settings.bloodSugarDangerousLow}";
                     _bloodSuger.text = $">{_settings.oxygenDangerousLevel}";
 
-                    _descripton.text = $"A slow heart rate below 22 BPM. The condition can be caused by the following:\n •\tStarving, low blood suger levels less than {_settings.bloodSugarDangerousLow} mg/dL. \n •\tLow Oxygen (SpO2) levels less then {_settings.oxygenDangerousLevel}%.\n •\tPre-existing heart ccondition.\n\nBeing Bradycardic for too long will result in Cardiac Arrest or a Heat Block. See furture entries for more infomation about treating these conditions.";
-                    _traetment.text = $"\n• Heart rate can be raised with Adrenaline.\n\n• If blood suger is low treat with food.\n\n • If SpO2 is low treat with oxygen.";
+                    _descripton.text = $"A slow heart rate below 22 BPM. The condition can be caused by the following:\n ï¿½\tStarving, low blood sugar levels less than {_settings.bloodSugarDangerousLow} mg/dL. \n ï¿½\tLow Oxygen (SpO2) levels less then {_settings.oxygenDangerousLevel}%.\n ï¿½\tPre-existing heart condition.\n\nBeing Bradycardic for too long will result in Cardiac Arrest or a Heat Block. See future entries for more information about treating these conditions.";
+                    _traetment.text = $"\nï¿½ Heart rate can be raised with Adrenaline.\n\nï¿½ If blood sugar is low treat with food.\n\n ï¿½ If SpO2 is low treat with oxygen.";
                     break;
                 case HeartCondition.HeartBlock:
-                    _heart.SetBlock(2);
+                    _heart.SetBlock(1);
 
-                    _descripton.text = $"A contition that causes the heart to skip a beat. On the EKG this is seen as a one or two extra p-waves (tiny waves) between beats. A Heart Block is caused by low CRP levels. This condition can cause Cardiac Arrest if left untreated.";
-                    _traetment.text = $"\n• Heart Block can be treated with Atropine.\n• After the patient must be treated with Ibuprofen to prevent further eposides.";
+                    _descripton.text = $"A condition that causes the heart to skip a beat. On the EKG this is seen as a one or two extra p-waves (tiny waves) between beats. A Heart Block is caused by low CRP levels. This condition can cause Cardiac Arrest if left untreated.";
+                    _traetment.text = $"\nï¿½ Heart Block can be treated with Atropine.\nï¿½ After the patient must be treated with Ibuprofen treated to prevent further episodes.";
 
                     break;
                 case HeartCondition.AtrialFibrillation:
                     _heart.SetAtrialFibrillation(true);
 
-                    _descripton.text = $"A contition that causes the heart to beat in irregular intervals. Atrial Fibrillation is caused by a elvated heart rate from elevate CRP levels. This condition can cause Cardiac Arrest if left untreated.";
-                    _traetment.text = $"\n• Atrial Fibrillation can be treated with Calcium Channel Blockers.\n• After the patient must be treated with Digoxin to prevent further eposides.";
+                    _descripton.text = $"A condition that causes the heart to beat in irregular intervals. Atrial Fibrillation is caused by a elevated heart rate from elevate CRP levels. This condition can cause Cardiac Arrest if left untreated.";
+                    _traetment.text = $"\nï¿½ Atrial Fibrillation can be treated with Calcium Channel Blockers.\nï¿½ After the patient must be treated with Digoxin treated to prevent further episodes.";
                     break;
                 case HeartCondition.VentricularFibrillation:
                     _heart.SetVentricularFibrillation(true);
 
-                    _descripton.text = $"A contition that causes the heart to contract in a abnormal heart rhythm. This looks like random noise on the EKG. This condition immediately cause a Cardiac Arrest.";
-                    _traetment.text = $"\n• Patients with Ventricular Fibrillationcan must be reivived with a defibrillator and then treated with then with Furosemide to prevent further eposides.";
+                    _descripton.text = $"A condition that causes the heart to contract in a abnormal heart rhythm. This looks like random noise on the EKG. This condition immediately cause a Cardiac Arrest.";
+                    _traetment.text = $"\nï¿½ Patients with Ventricular Fibrillation can must be revived with a defibrillator and then treated with then with Furosemide to prevent further episodes.";
                     break;
                 case HeartCondition.Hypothermia:
                     _heart.SetJWave(1f);
 
-                    _descripton.text = $"The patients body temperture is lower than 30 degrees. Hypothermia causes an adnormal EKG which can be seen as a second peak (J-Wave). This condition can cause a Cardiac Arrest if left untreated.";
-                    _traetment.text = $"\n• Patients with Hypothermia can be treated by rasing their room temperture.";
+                    _descripton.text = $"The patients body temperature is lower than 30 degrees. Hypothermia causes an abnormal EKG which can be seen as a second peak (J-Wave). This condition can cause a Cardiac Arrest if left untreated.";
+                    _traetment.text = $"\nï¿½ Patients with Hypothermia can be treated by raising their room temperature using the thermostat in their room.";
                     break;
                 case HeartCondition.CardiacArrest:
                     _heart.CauseCardiacArrest(true);
 
                     _descripton.text = $"The heart stops and is no longer functional.";
-                    _traetment.text = $"\n• Patients with Cardiac Arrest can be revived with a defibrillator. The condition that caused the Cardiac Arrest must be treat to prevent further eposides.";
+                    _traetment.text = $"\nï¿½ Patients with Cardiac Arrest can be revived with a defibrillator. The condition that caused the Cardiac Arrest must be treat to prevent further episodes.";
                     break;
 
             }
@@ -151,7 +141,7 @@ namespace CodeBlack
             return _pos != 0;
         }
 
-        private void Awake()
+        private void OnEnable()
         {
             Show((HeartCondition)_pos);
         }
@@ -161,7 +151,7 @@ namespace CodeBlack
             _bpm.text = ((int)_heart.Bpm()).ToString();
             _bloodPressure.text = $"{(int)_heart.Sbp()}/{(int)_heart.Dbp()}";
 
-            if (_heart.IsDead() && _last != HeartCondition.CardiacArrest && _last != HeartCondition.VentricularFibrillation)
+            if (_heart.IsDead() && _last != HeartCondition.CardiacArrest && _last != HeartCondition.Bradycardia)
             {
                 _heart.Revive();
             }
